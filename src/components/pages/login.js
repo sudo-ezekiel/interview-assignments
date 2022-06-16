ff6547022e0a1f52d8887b116da9906cb866afca
sudo-ezekiel <sudo-ezekiel@users.noreply.github.com>
2022-06-16 19:09:28 +0300

    [qnr-front-end-assignment] login function created

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,13 +1,23 @@
 import { useState } from "react";
-import TextField from '@mui/material/TextField';
+import { TextField, Box, Button } from '@mui/material';
 
 export const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    const handleLogin = e => {
+        e.preventDefault();
+        console.log("username is:", username, "password is:", password)
+    }
+
     return (
-        <div>
-            <TextField id="standard-basic" label="Standard" variant="standard" />
-            <TextField id="standard-basic" label="Standard" variant="standard" />
-        </div>
+        <Box sx={{ m: 'auto', width: '50vw', display: 'flex', flexWrap: 'wrap' }}
+            component="form"
+            autoComplete="off"
+            onSubmit={handleLogin}>
+            <TextField id="username" value={username} onChange={e => setUsername(e.target.value)} autoComplete="off" fullWidth required label="Username" variant="standard" />
+            <TextField id="password" value={password} onChange={e => setPassword(e.target.value)} autoComplete="off" fullWidth type="password" required label="Password" variant="standard" />
+            <Button type="submit" sx={{ m: 'auto' }}>Submit</Button>
+        </Box>
     )
 }
