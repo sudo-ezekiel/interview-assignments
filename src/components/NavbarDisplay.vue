f06bff26c18363f20b79c864d3225504841ef86c
sudo-ezekiel <sudo-ezekiel@users.noreply.github.com>
2023-12-27 05:27:59 +0200

    [improvit-front-end-assignment] created navbar based on design

--- src/components/NavbarDisplay.vue
+++ src/components/NavbarDisplay.vue
@@ -1,11 +1,35 @@
 <template>
-    <div>
-        Vue Navbar Comp
+    <div class="navbar-wrapper">
+        <div class="navbar">
+            <img src="../assets/improvITBlack.png">
+            <div>
+                <h4 class="page-title">Team Members</h4>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
+
+<style>
+.navbar-wrapper {
+    border-top: 2rem solid #00a3e4;
+    border-bottom: 0.4rem solid #e5e5e5;
+}
+
+.navbar {
+    padding: 2rem;
+    display: flex;
+    flex-direction: row;
+    gap: 1.2rem;
+}
+
+.page-title {
+    color: #b1b1b1;
+    font-size: xx-large;
+}
+</style>
