687ff01d109a354108aed608bf1d640bcc255c2e
sudo-ezekiel <sudo-ezekiel@users.noreply.github.com>
2024-06-24 18:51:27 +0300

    [sleed-front-end-assignment] image optimizer and fix image sections

--- components/home/halfSizeImagesSection.js
+++ components/home/halfSizeImagesSection.js
@@ -1,11 +1,18 @@
+import { prefix } from "@/lib/prefix";
+
 const HalfSizeImages = () => {
   return (
     <div className="jumbotron text-white bg-black">
-      <img src="/assets/leftHalfSize.png" className="homePageHalfSizeImages" />
-      <img src="/assets/rightHalfSize.png" className="homePageHalfSizeImages" />
+      <img
+        src={`${prefix}/assets/leftHalfSize.png`}
+        className="homePageHalfSizeImages"
+      />
+      <img
+        src={`${prefix}/assets/rightHalfSize.png`}
+        className="homePageHalfSizeImages"
+      />
     </div>
   );
 };
 
 export default HalfSizeImages;
-
