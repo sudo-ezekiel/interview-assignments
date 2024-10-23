7611c107433629416f65ab702a0dd58f17b00834
sudo-ezekiel <sudo-ezekiel@users.noreply.github.com>
2024-10-23 11:15:44 +0300

    [harbor-lab-front-end-assignment] base test case

--- cypress/e2e/1-flow-testing/flow.cy.js
+++ cypress/e2e/1-flow-testing/flow.cy.js
@@ -1,44 +1,29 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
-
 describe("Hacker News App", () => {
-  let storyId = "";
-
   before(() => {
     cy.visit("http://localhost:3000");
   });
 
-  // async cause I have to await so I can save storyId and not be undefined
-  it("searches for stories, saves the first one, and retrieves its story_id", async () => {
+  it("Checking if user flow works", () => {
+    let storyId = 25956076;
+
     cy.intercept(
       "GET",
-      "https://hn.algolia.com/api/v1/search?query=REACT%20CYPRESS",
+      "https://hn.algolia.com/api/v1/search?query=Comprehensive%20Intro%20to%20Testing%20JavaScript%20and%20React%20(Jest,%20Cypress,%20RTL)",
     ).as("getStories");
 
-    cy.get('[data-testid="search-input"]').type("REACT CYPRESS");
+    cy.get('[data-testid="search-input"]').type(
+      "Comprehensive Intro to Testing JavaScript and React (Jest, Cypress, RTL)",
+    );
 
     cy.wait("@getStories");
 
     cy.get('[data-testid="search-results"]').should("exist");
 
-    // Get the first story's element ID that contains the story_id
-    storyId = await cy
-      .get('[data-testid^="search-results-list-li-"]')
-      .first()
-      .then(($firstStory) => {
-        const elementId = $firstStory[0].id;
-        return elementId.split("-").pop();
-      });
+    cy.get(`[data-testid="search-results-list-li-${storyId}"]`).click();
+
+    cy.get('[data-testid="saved-stories-list"]').should("contain", "React");
 
     cy.log(`Saved story ID: ${storyId}`);
   });
